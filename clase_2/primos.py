e1d31f0d7f86dfa3f741d1e16817af19c4099272
cerd0c0nej0 <abicita.i.u@gmail.com>
2022-02-01 17:09:29 -0400

    primos

---
+++ clase_2/primos.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  1 16:43:37 2022
+
+@author: LENOVO
+"""
+list = []
+n = 1   #valores del 1 al 100
+while n <= 100: #bucle de numeros de 1 al 100
+    c = 1 # contador de divisore
+    x = 0 # incremento divisor
+    while c <= n:
+        if n % c == 0: 
+            x = x + 1
+        c = c + 1
+    if x == 2:
+        list.append(n)       
+    n = n + 1 # incrmeneto valor  
+print(" los numeros primos del 1 al 100 son :",list)
